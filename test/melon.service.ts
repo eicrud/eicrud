78bbe9b946fd5eaeaed826e9cd09cf7d61b6a933
acrosett <>
2024-06-04 18:01:59 +0200

    test search command limit & batchable

--- test/melon.service.ts
+++ test/melon.service.ts
@@ -2,14 +2,49 @@ import { ModuleRef } from '@nestjs/core';
 
 import { Melon } from './entities/Melon';
 import { MyConfigService } from './myconfig.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CrudService } from '../core/crud/crud.service';
 import { CrudSecurity } from '../core/config/model/CrudSecurity';
 import { CrudUser } from '../core/config/model/CrudUser';
+import { IsOptional, IsString } from 'class-validator';
+import { FindResponseDto } from '../shared/interfaces';
+import { UserProfile } from './entities/UserProfile';
+import { CrudContext } from '../core/crud/model/CrudContext';
+import {
+  $MaxLength,
+  $MaxSize,
+  $Transform,
+  $Type,
+} from '../core/validation/decorators';
+import { CrudErrors } from '../shared/CrudErrors';
+export class SearchMelonDto {
+  @IsString()
+  @$Transform((v: string) => v.replace(/[.*+?^$}{)(|[\]\\]/g, '\\$&'))
+  nameLike: string;
 
+  @IsOptional()
+  @IsString({ each: true })
+  @$MaxSize(-1)
+  ids?: string[];
+
+  @IsOptional()
+  @IsString()
+  ownerEmail?: string;
+}
 const melonSecurity = (MELON) => {
   return {
-    cmdSecurityMap: {},
+    cmdSecurityMap: {
+      search: {
+        dto: SearchMelonDto,
+        rolesRights: {
+          guest: {
+            async defineCMDAbility(can, cannot, ctx) {
+              can('search', MELON);
+            },
+          },
+        },
+      },
+    },
 
     rolesRights: {
       super_admin: {
@@ -46,5 +81,49 @@ export class MelonService extends CrudService<Melon> {
   constructor(protected moduleRef: ModuleRef) {
     const serviceName = CrudService.getName(Melon);
     super(moduleRef, Melon, melonSecurity(serviceName));
+  }
+
+  async $search(
+    dto: SearchMelonDto,
+    ctx: CrudContext,
+    inheritance?: any,
+  ): Promise<FindResponseDto<Melon>> {
+    const query: Partial<Melon> = {};
+
+    if (dto.nameLike) {
+      query.name = new RegExp(dto.nameLike, 'i') as any;
+    }
+
+    if (dto.ids) {
+      const maxBatchSize = 199;
+      if (dto.ids.length > maxBatchSize) {
+        throw new BadRequestException(
+          CrudErrors.MAX_BATCH_SIZE_EXCEEDED.str({
+            maxBatchSize,
+            batchSize: dto.ids.length,
+            field: 'ids',
+          }),
+        );
+      }
+    }
+
+    if (dto.ownerEmail) {
+      query.ownerEmail = dto.ownerEmail;
+    }
+
+    const fakeCtx: CrudContext = {
+      ...ctx,
+      query,
+      data: null,
+      origin: 'crud',
+      method: 'GET',
+    };
+
+    await this.crudAuthorization.authorize(fakeCtx, this.security);
+
+    if (dto.ids) {
+      return this.$findIn(dto.ids, query, fakeCtx, inheritance);
+    }
+    return this.$find(query, fakeCtx, inheritance);
   }
 }
--- test/profile.service.ts
+++ test/profile.service.ts
@@ -15,6 +15,7 @@ import {
   $Transform,
   $Type,
 } from '../core/validation/decorators';
+import { FindResponseDto } from '../shared/interfaces';
 
 class subTestCmdDto {
   @IsString()
@@ -36,9 +37,29 @@ export class TestCmdDto {
   forbiddenField?: string;
 }
 
+export class SearchCmdDto {
+  @IsString()
+  @$Transform((v: string) => v.replace(/[.*+?^$}{)(|[\]\\]/g, '\\$&'))
+  userNameLike: string;
+
+  @IsOptional()
+  @IsString()
+  type?: string;
+}
+
 const myProfileSecurity = (USER_PROFILE) => {
   return {
     cmdSecurityMap: {
+      search: {
+        dto: SearchCmdDto,
+        rolesRights: {
+          guest: {
+            async defineCMDAbility(can, cannot, ctx) {
+              can('search', USER_PROFILE);
+            },
+          },
+        },
+      },
       testCmd: {
         maxUsesPerUser: 10,
         additionalUsesPerTrustPoint: 1,
@@ -131,5 +152,31 @@ export class MyProfileService extends CrudService<UserProfile> {
   ): Promise<string> {
     let res = dto?.sub?.subfield || dto.returnMessage;
     return Promise.resolve(res);
+  }
+
+  async $search(
+    dto: SearchCmdDto,
+    ctx: CrudContext,
+    inheritance?: any,
+  ): Promise<FindResponseDto<UserProfile>> {
+    const query: Partial<UserProfile> = {
+      userName: new RegExp(dto.userNameLike, 'i') as any,
+    };
+
+    if (dto.type) {
+      query.type = dto.type as any;
+    }
+
+    const fakeCtx: CrudContext = {
+      ...ctx,
+      query,
+      data: null,
+      origin: 'crud',
+      method: 'GET',
+    };
+
+    await this.crudAuthorization.authorize(fakeCtx, this.security);
+
+    return this.$find(query, fakeCtx, inheritance);
   }
 }
