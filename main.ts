79c7caee5865dd077c234bf592ffa244deabea91
acrosett <104265464+acrosett@users.noreply.github.com>
2024-06-12 21:34:04 +0200

    test ci

--- main.ts
+++ main.ts
@@ -5,15 +5,8 @@ import { FastifyAdapter } from '@nestjs/platform-fastify';
 async function bootstrap() {
   const app = await NestFactory.create(TestModule, new FastifyAdapter());
   const PORT = process.env.PORT || 3000;
-
-  process.on('SIGTERM', async () => {
-    if (process.env.GRACEFUL_EXIT) {
-      console.log('EXITING GRACEFULLY...');
-      process.exit(0);
-    }
-  });
-
   console.log(`Listening on port ${PORT}`);
+
   await app.listen(PORT);
 }
 
