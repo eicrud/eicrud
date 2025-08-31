572f1e8d75c4e946c7b74329afcda038d96c7ead
acrosett <104265464+acrosett@users.noreply.github.com>
2025-08-31 18:46:40 +0200

    feat(cli): ensure DB schema on startup in main.ts

--- cli/actions/Setup.ts
+++ cli/actions/Setup.ts
@@ -234,13 +234,19 @@ export class Setup {
     let mainFileContent = fs.readFileSync(mainFile, 'utf8');
     const mainFileImports = [
       "import { FastifyAdapter } from '@nestjs/platform-fastify';",
+      "import { MikroORM } from '@mikro-orm/core';",
     ];
     mainFileContent =
       mainFileImports.join('\n') +
       '\n' +
       mainFileContent.replace(
-        'NestFactory.create(AppModule)',
-        'NestFactory.create(AppModule, new FastifyAdapter())',
+        'NestFactory.create(AppModule);',
+        `NestFactory.create(AppModule, new FastifyAdapter());
+  
+  const orm = app.get(MikroORM);
+  const generator = orm.getSchemaGenerator();
+  await generator.ensureDatabase();
+  await generator.createSchema();`,
       );
     fs.writeFileSync(mainFile, mainFileContent);
     console.log('UPDATED:', mainFile);
