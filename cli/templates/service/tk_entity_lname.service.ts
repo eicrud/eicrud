e8609f09107bdb296ff7afcfcc49f3fb62bec44a
acrosett <104265464+acrosett@users.noreply.github.com>
2025-09-15 19:00:37 +0200

    feat(core): change service import of ms subfolder config (generated by CLI)

--- cli/templates/service/tk_entity_lname.service.ts
+++ cli/templates/service/tk_entity_lname.service.ts
@@ -5,13 +5,17 @@ import { getSecurity } from "./tk_entity_lname.security";
 import { CrudService, Inheritance, CrudContext } from "@eicrud/core/crud";
 import { serviceCmds } from "./cmds";
 import { hooks } from "./tk_entity_lname.hooks";
-tk_service_config_import
 
 @Injectable()
 export class tk_entity_nameService extends CrudService<tk_entity_name> {
     constructor(protected moduleRef: ModuleRef) {
         const serviceName = CrudService.getName(tk_entity_name);
-        super(moduleRef, tk_entity_name, getSecurity(serviceName), tk_service_config_usage);
+        super(moduleRef, tk_entity_name, getSecurity(serviceName), { hooks });
+    }
+
+    async onModuleInit(): Promise<void> {
+        tk_service_config_import
+        await super.onModuleInit();
     }
     
     // GENERATED START - do not remove
